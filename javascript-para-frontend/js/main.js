255e17201bff281414cb012e866d9b55410abf80
jvieira <contato.josevictorvieira@gmail.com>
2023-02-15 18:06:23 -0300

    Adicionando branch main

--- javascript-para-frontend/js/main.js
+++ javascript-para-frontend/js/main.js
@@ -1,6 +1,7 @@
-
-
-
-function Play(){
-    
+function PlayPom(){
+    document.querySelector("#som_tecla_pom").play();
 }
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+listaDeTeclas[0].oneclick = 
