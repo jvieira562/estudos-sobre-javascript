f47d48f5724ff1e535a92d55848f9c93e48476a6
José Victor Vieira <contato.josevictorvieira@gmail.com>
2023-02-16 00:38:35 -0300

    Finalizando curso 1

--- javascript-para-frontend/js/main.js
+++ javascript-para-frontend/js/main.js
@@ -1,6 +1,39 @@
+function TocaSom(seletorDeAudio)  
+{
+    const elemento = document.querySelector(seletorDeAudio)
 
 
+    if((elemento != null) && (elemento.localName == 'audio'))
+    {
+        
+        document.querySelector(seletorDeAudio).play();
+    } 
+    else 
+    {
+        console.log("Elemento invalido!")
+    }
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function Play(){
-    
+for(let i = 0; i < listaDeTeclas.length; i++)
+{
+    let som = listaDeTeclas[i].classList[1];
+    listaDeTeclas[i].onclick = 
+    function () 
+    { 
+        TocaSom(`#som_${som}`); 
+    }
+
+    listaDeTeclas[i].onkeydown = function(event)
+    {
+        if(event.code === 'Space' || event.code === 'Enter') 
+        {
+            listaDeTeclas[i].classList.add('ativa');
+        }        
+    }
+
+    listaDeTeclas[i].onkeyup = function()
+    {
+        listaDeTeclas[i].classList.remove('ativa');
+    }
 }
